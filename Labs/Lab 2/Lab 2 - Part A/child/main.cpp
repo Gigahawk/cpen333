b334e6729b3b0e75aa4ae5fee17d9f28fdb5d996
Jasper Chan <jasperchan515@gmail.com>
2020-09-18 19:14:59 -0700

    Lab 2 Part A

---
+++ Labs/Lab 2/Lab 2 - Part A/child/main.cpp
@@ -0,0 +1,25 @@
+#include "../rt.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int id = 0;
+	int count = 200;
+	const char* default_name = "World";
+	char* name;
+	if (argc >= 2)
+		id = atoi(argv[1]);
+	if (argc >= 3)
+		count = atoi(argv[2]);
+	if (argc >= 4)
+		name = argv[3];
+	else
+		name = (char*)default_name;
+	for (int i = 0; i < count; i++) {
+		cout << "[CHILD:" << id << "] Hello " << name << " " << i <<"\n";
+		Sleep(50);
+	}
+	return 0;
+}
